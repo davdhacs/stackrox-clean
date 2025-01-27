stackrox/stackrox

chore(nodescan): misc cleanup

sensor/common/compliance/node_inventory_handler_impl.go
@@ -21,7 +21,7 @@ var (
 
 type nodeInventoryHandlerImpl struct {
 	inventories  <-chan *storage.NodeInventory
-	reportWraps  <-chan *index.IndexReportWrap
+	indexReports <-chan *index.IndexReport
 	toCentral    <-chan *message.ExpiringMessage
 	centralReady concurrency.Signal
 	// acksFromCentral is for connecting the replies from Central with the toCompliance chan
@@ -160,7 +160,7 @@ func (c *nodeInventoryHandlerImpl) nodeInventoryHandlingLoop(toCentral chan *mes
 				log.Debugf("Mapping NodeInventory name '%s' to Node ID '%s'", inventory.GetNodeName(), nodeID)
 				c.sendNodeInventory(toCentral, inventory)
 			}
-		case wrap, ok := <-c.reportWraps:
+		case indexReport, ok := <-c.indexReports:
 			if !ok {
 				log.Warn("Report wraps channel is closed")
 				c.stopper.Flow().StopWithError(errInputChanClosed)
@@ -170,16 +170,16 @@ func (c *nodeInventoryHandlerImpl) nodeInventoryHandlingLoop(toCentral chan *mes
 				log.Warn("Received Index Report but Central is not reachable. Skipping for now")
 				continue
 			}
-			if wrap == nil || wrap.IndexReport == nil {
+			if indexReport == nil || indexReport.IndexReport == nil {
 				log.Warn("Received nil index report: not sending to Central")
 				break
 			}
-			if nodeID, err := c.nodeMatcher.GetNodeID(wrap.NodeName); err != nil {
-				log.Warnf("Node %q unknown to Sensor.", wrap.NodeName)
+			if nodeID, err := c.nodeMatcher.GetNodeID(indexReport.NodeName); err != nil {
+				log.Warnf("Node %q unknown to Sensor.", indexReport.NodeName)
 			} else {
 				log.Debugf("Sending Index Report to Central")
-				wrap.NodeID = nodeID
-				c.sendNodeIndex(toCentral, wrap)
+				indexReport.NodeID = nodeID
+				c.sendNodeIndex(toCentral, indexReport)
 			}
 		}
 	}
@@ -220,8 +220,8 @@ func (c *nodeInventoryHandlerImpl) sendNodeInventory(toC chan<- *message.Expirin
 	}
 }
 
-func (c *nodeInventoryHandlerImpl) sendNodeIndex(toC chan<- *message.ExpiringMessage, indexWrap *index.IndexReportWrap) {
-	if indexWrap == nil || indexWrap.IndexReport == nil {
+func (c *nodeInventoryHandlerImpl) sendNodeIndex(toC chan<- *message.ExpiringMessage, indexReport *index.IndexReport) {
+	if indexReport == nil || indexReport.IndexReport == nil {
 		return
 	}
 
@@ -230,10 +230,10 @@ func (c *nodeInventoryHandlerImpl) sendNodeIndex(toC chan<- *message.ExpiringMes
 	case toC <- message.New(&central.MsgFromSensor{
 		Msg: &central.MsgFromSensor_Event{
 			Event: &central.SensorEvent{
-				Id:     indexWrap.NodeID,
+				Id:     indexReport.NodeID,
 				Action: central.ResourceAction_UNSET_ACTION_RESOURCE,
 				Resource: &central.SensorEvent_IndexReport{
-					IndexReport: indexWrap.IndexReport,
+					IndexReport: indexReport.IndexReport,
 				},
 			},
 		},

davdhacs/stackrox-clean

sensor/common/compliance/node_inventory_handler_impl.go
@@ -34,7 +34,7 @@ const (
 
 type nodeInventoryHandlerImpl struct {
 	inventories  <-chan *storage.NodeInventory
-	reportWraps  <-chan *index.IndexReportWrap
+	indexReports <-chan *index.IndexReport
 	toCentral    <-chan *message.ExpiringMessage
 	centralReady concurrency.Signal
 	// acksFromCentral is for connecting the replies from Central with the toCompliance chan
@@ -161,7 +161,7 @@ func (c *nodeInventoryHandlerImpl) run() (toCentral <-chan *message.ExpiringMess
 					return
 				}
 				c.handleNodeInventory(inventory, ch2Central)
-			case wrap, ok := <-c.reportWraps:
+			case wrap, ok := <-c.indexReports:
 				if !ok {
 					c.stopper.Flow().StopWithError(errIndexInputChanClosed)
 					return
@@ -207,7 +207,7 @@ func (c *nodeInventoryHandlerImpl) handleNodeInventory(
 }
 
 func (c *nodeInventoryHandlerImpl) handleNodeIndex(
-	index *index.IndexReportWrap,
+	index *index.IndexReport,
 	toCentral chan *message.ExpiringMessage,
 ) {
 	if index == nil || index.IndexReport == nil {
@@ -290,7 +290,7 @@ func (c *nodeInventoryHandlerImpl) sendNodeInventory(toC chan<- *message.Expirin
 	}
 }
 
-func (c *nodeInventoryHandlerImpl) sendNodeIndex(toC chan<- *message.ExpiringMessage, indexWrap *index.IndexReportWrap) {
+func (c *nodeInventoryHandlerImpl) sendNodeIndex(toC chan<- *message.ExpiringMessage, indexWrap *index.IndexReport) {
 	if indexWrap == nil || indexWrap.IndexReport == nil {
 		log.Debugf("Empty IndexReport - not sending to Central")
 		return