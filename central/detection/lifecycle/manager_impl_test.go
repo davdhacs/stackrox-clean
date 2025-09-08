stackrox/stackrox

Helm config is used if the manage type is not manual

central/detection/lifecycle/manager_impl_test.go
@@ -27,17 +27,23 @@ import (
 
 var (
 	clusterAutolockEnabled = &storage.Cluster{
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutolockProcessBaseline: &storage.AutolockProcessBaseline{
-				Enabled: true,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+					Enabled: true,
+				},
 			},
 		},
 	}
 
 	clusterAutolockDisabled = &storage.Cluster{
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutolockProcessBaseline: &storage.AutolockProcessBaseline{
-				Enabled: false,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+					Enabled: false,
+				},
 			},
 		},
 	}

davdhacs/stackrox-clean

central/detection/lifecycle/manager_impl_test.go
@@ -25,19 +25,23 @@ import (
 
 var (
 	clusterAutolockEnabled = &storage.Cluster{
-		ManagedBy: storage.ManagerType_MANAGER_TYPE_MANUAL,
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutoLockProcessBaselines: &storage.AutoLockProcessBaselines{
-				Enabled: true,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaselines{
+					Enabled: true,
+				},
 			},
 		},
 	}
 
 	clusterAutolockDisabled = &storage.Cluster{
-		ManagedBy: storage.ManagerType_MANAGER_TYPE_MANUAL,
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutoLockProcessBaselines: &storage.AutoLockProcessBaselines{
-				Enabled: true,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaselines{
+					Enabled: false,
+				},
 			},
 		},
 	}