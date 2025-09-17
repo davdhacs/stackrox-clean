stackrox/stackrox

Helm config is used if the manage type is not manual

central/detection/lifecycle/manager_impl_test.go
@@ -27,17 +27,23 @@ import (
 
 var (
 	clusterAutolockEnabled = &storage.Cluster{
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutolockProcessBaseline: &storage.AutolockProcessBaseline{
-				Enabled: true,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+					Enabled: true,
+				},
 			},
 		},
 	}
 
 	clusterAutolockDisabled = &storage.Cluster{
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutolockProcessBaseline: &storage.AutolockProcessBaseline{
-				Enabled: false,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+					Enabled: false,
+				},
 			},
 		},
 	}

davdhacs/stackrox-clean

central/detection/lifecycle/manager_impl_test.go
@@ -27,19 +27,23 @@ import (
 
 var (
 	clusterAutolockEnabled = &storage.Cluster{
-		ManagedBy: storage.ManagerType_MANAGER_TYPE_MANUAL,
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutoLockProcessBaselinesConfig: &storage.AutoLockProcessBaselinesConfig{
-				Enabled: true,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaselinesConfig{
+					Enabled: true,
+				},
 			},
 		},
 	}
 
 	clusterAutolockDisabled = &storage.Cluster{
-		ManagedBy: storage.ManagerType_MANAGER_TYPE_MANUAL,
-		DynamicConfig: &storage.DynamicClusterConfig{
-			AutoLockProcessBaselinesConfig: &storage.AutoLockProcessBaselinesConfig{
-				Enabled: false,
+		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
+		HelmConfig: &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AutolockProcessBaseline: &storage.AutolockProcessBaselinesConfig{
+					Enabled: false,
+				},
 			},
 		},
 	}