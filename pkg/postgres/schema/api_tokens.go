sluetze/stackrox

Rename unicity constraint

pkg/postgres/schema/api_tokens.go
@@ -45,7 +45,7 @@ const (
 // APITokens holds the Gorm model for Postgres table `api_tokens`.
 type APITokens struct {
 	ID         string     `gorm:"column:id;type:varchar;primaryKey"`
-	Name       string     `gorm:"column:name;type:varchar;uniqueIndex:apitokens_unique_name_indicator"`
+	Name       string     `gorm:"column:name;type:varchar;uniqueIndex:api_tokens_unique_name"`
 	Expiration *time.Time `gorm:"column:expiration;type:timestamp"`
 	Revoked    bool       `gorm:"column:revoked;type:bool"`
 	Serialized []byte     `gorm:"column:serialized;type:bytea"`

davdhacs/stackrox-clean

pkg/postgres/schema/api_tokens.go
@@ -45,6 +45,7 @@ const (
 // APITokens holds the Gorm model for Postgres table `api_tokens`.
 type APITokens struct {
 	ID         string     `gorm:"column:id;type:varchar;primaryKey"`
+	Name       string     `gorm:"column:name;type:varchar;uniqueIndex:apitokens_name"`
 	Expiration *time.Time `gorm:"column:expiration;type:timestamp"`
 	Revoked    bool       `gorm:"column:revoked;type:bool"`
 	Serialized []byte     `gorm:"column:serialized;type:bytea"`