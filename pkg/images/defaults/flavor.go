4310cdcafcc58b2bc6e32881de0b2916d411e7b0
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-12-01 11:15:05 -0700

    remove from pkg defaults

--- pkg/images/defaults/flavor.go
+++ pkg/images/defaults/flavor.go
@@ -150,27 +150,27 @@ func RHACSReleaseImageFlavor() ImageFlavor {
 	v := version.GetAllVersionsUnified()
 	return ImageFlavor{
 		MainRegistry:       "registry.redhat.io/advanced-cluster-security",
-		MainImageName:      "rhacs-main-rhel8",
+		MainImageName:      "rhacs-main",
 		MainImageTag:       v.MainVersion,
 		CentralDBImageTag:  v.MainVersion,
-		CentralDBImageName: "rhacs-central-db-rhel8",
+		CentralDBImageName: "rhacs-central-db",
 
 		CollectorRegistry:  "registry.redhat.io/advanced-cluster-security",
-		CollectorImageName: "rhacs-collector-rhel8",
+		CollectorImageName: "rhacs-collector",
 		CollectorImageTag:  v.CollectorVersion,
 
 		FactRegistry:  "registry.redhat.io/advanced-cluster-security",
-		FactImageName: "rhacs-fact-rhel8",
+		FactImageName: "rhacs-fact",
 		FactImageTag:  v.FactVersion,
 
-		ScannerImageName:       "rhacs-scanner-rhel8",
-		ScannerSlimImageName:   "rhacs-scanner-slim-rhel8",
+		ScannerImageName:       "rhacs-scanner",
+		ScannerSlimImageName:   "rhacs-scanner-slim",
 		ScannerImageTag:        v.ScannerVersion,
-		ScannerDBImageName:     "rhacs-scanner-db-rhel8",
-		ScannerDBSlimImageName: "rhacs-scanner-db-slim-rhel8",
+		ScannerDBImageName:     "rhacs-scanner-db",
+		ScannerDBSlimImageName: "rhacs-scanner-db-slim",
 
-		ScannerV4ImageName:   "rhacs-scanner-v4-rhel8",
-		ScannerV4DBImageName: "rhacs-scanner-v4-db-rhel8",
+		ScannerV4ImageName:   "rhacs-scanner-v4",
+		ScannerV4DBImageName: "rhacs-scanner-v4-db",
 		// Scanner v4 is released along with the main image, so the tags are expected to be the same.
 		ScannerV4ImageTag: v.MainVersion,
 
