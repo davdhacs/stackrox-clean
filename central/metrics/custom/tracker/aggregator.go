stackrox/stackrox

include and exclude filters

central/metrics/custom/tracker/aggregator.go
@@ -36,18 +36,19 @@ type aggregatedRecord struct {
 //			{"Z": {labels: {L2="Z"}, total: 2}}
 //	}
 type aggregator[F Finding] struct {
-	result  map[MetricName]map[aggregationKey]*aggregatedRecord
-	md      MetricDescriptors
-	lf      LabelFilters
-	getters LazyLabelGetters[F]
+	result         map[MetricName]map[aggregationKey]*aggregatedRecord
+	md             MetricDescriptors
+	includeFilters LabelFilters
+	excludeFilters LabelFilters
+	getters        LazyLabelGetters[F]
 }
 
-func makeAggregator[F Finding](md MetricDescriptors, lf LabelFilters, getters LazyLabelGetters[F]) *aggregator[F] {
+func makeAggregator[F Finding](md MetricDescriptors, includeFilters, excludeFilters LabelFilters, getters LazyLabelGetters[F]) *aggregator[F] {
 	aggregated := make(map[MetricName]map[aggregationKey]*aggregatedRecord)
 	for metric := range md {
 		aggregated[metric] = make(map[aggregationKey]*aggregatedRecord)
 	}
-	return &aggregator[F]{aggregated, md, lf, getters}
+	return &aggregator[F]{aggregated, md, includeFilters, excludeFilters, getters}
 }
 
 // count the finding in the aggregation result.
@@ -58,9 +59,14 @@ func (a *aggregator[F]) count(finding F) {
 	}
 
 	for metric, labels := range a.md {
-		// Apply label filters. It could, e.g., keep only "ACTIVE" alerts.
-		if !a.pass(finding, a.lf[metric]) {
+		// Apply include filters. It could, e.g., keep only "ACTIVE" alerts.
+		if !a.matchInclude(finding, a.includeFilters[metric]) {
 			// Ignore this finding for this metric.
+			continue
+		}
+		// Apply exclude filters. It could, e.g., drop "LOW_SEVERITY" alerts.
+		if a.matchExclude(finding, a.excludeFilters[metric]) {
+			// Drop this finding for this metric.
 			continue
 		}
 
@@ -73,14 +79,26 @@ func (a *aggregator[F]) count(finding F) {
 	}
 }
 
-// pass checks if the finding labels pass the filters.
-func (a *aggregator[F]) pass(finding F, filters map[Label]*regexp.Regexp) bool {
+// matchInclude checks if the finding labels pass the include filters.
+// Returns true if all label values match the according filter patterns.
+func (a *aggregator[F]) matchInclude(finding F, filters map[Label]*regexp.Regexp) bool {
 	for label, pattern := range filters {
 		if !pattern.MatchString(a.getters[label](finding)) {
 			return false
 		}
 	}
 	return true
+}
+
+// matchExclude checks if the finding labels match any exclude filter.
+// Returns true if any label value matches its according filter pattern.
+func (a *aggregator[F]) matchExclude(finding F, filters map[Label]*regexp.Regexp) bool {
+	for label, pattern := range filters {
+		if pattern.MatchString(a.getters[label](finding)) {
+			return true
+		}
+	}
+	return false
 }
 
 // makeAggregationKey computes an aggregation key according to the provided

davdhacs/stackrox-clean

central/metrics/custom/tracker/aggregator.go
@@ -36,18 +36,19 @@ type aggregatedRecord struct {
 //			{"Z": {labels: {L2="Z"}, total: 2}}
 //	}
 type aggregator[F Finding] struct {
-	result  map[MetricName]map[aggregationKey]*aggregatedRecord
-	md      MetricDescriptors
-	lf      LabelFilters
-	getters LazyLabelGetters[F]
+	result         map[MetricName]map[aggregationKey]*aggregatedRecord
+	md             MetricDescriptors
+	includeFilters LabelFilters
+	excludeFilters LabelFilters
+	getters        LazyLabelGetters[F]
 }
 
-func makeAggregator[F Finding](md MetricDescriptors, lf LabelFilters, getters LazyLabelGetters[F]) *aggregator[F] {
+func makeAggregator[F Finding](md MetricDescriptors, includeFilters, excludeFilters LabelFilters, getters LazyLabelGetters[F]) *aggregator[F] {
 	result := make(map[MetricName]map[aggregationKey]*aggregatedRecord)
 	for metric := range md {
 		result[metric] = make(map[aggregationKey]*aggregatedRecord)
 	}
-	return &aggregator[F]{result, md, lf, getters}
+	return &aggregator[F]{result, md, includeFilters, excludeFilters, getters}
 }
 
 // count the finding in the aggregation result.
@@ -58,9 +59,14 @@ func (a *aggregator[F]) count(finding F) {
 	}
 
 	for metric, labels := range a.md {
-		// Apply label filters. It could, e.g., keep only "ACTIVE" alerts.
-		if !a.pass(finding, a.lf[metric]) {
+		// Apply include filters. It could, e.g., keep only "ACTIVE" alerts.
+		if !a.matchInclude(finding, a.includeFilters[metric]) {
 			// Ignore this finding for this metric.
+			continue
+		}
+		// Apply exclude filters. It could, e.g., drop "LOW_SEVERITY" alerts.
+		if a.matchExclude(finding, a.excludeFilters[metric]) {
+			// Drop this finding for this metric.
 			continue
 		}
 
@@ -73,14 +79,26 @@ func (a *aggregator[F]) count(finding F) {
 	}
 }
 
-// pass checks if the finding labels pass the filters.
-func (a *aggregator[F]) pass(finding F, filters map[Label]*regexp.Regexp) bool {
+// matchInclude checks if the finding labels pass the include filters.
+// Returns true if all label values match the according filter patterns.
+func (a *aggregator[F]) matchInclude(finding F, filters map[Label]*regexp.Regexp) bool {
 	for label, pattern := range filters {
 		if !pattern.MatchString(a.getters[label](finding)) {
 			return false
 		}
 	}
 	return true
+}
+
+// matchExclude checks if the finding labels match any exclude filter.
+// Returns true if any label value matches its according filter pattern.
+func (a *aggregator[F]) matchExclude(finding F, filters map[Label]*regexp.Regexp) bool {
+	for label, pattern := range filters {
+		if pattern.MatchString(a.getters[label](finding)) {
+			return true
+		}
+	}
+	return false
 }
 
 // reset clears the aggregation result without reallocating the maps.