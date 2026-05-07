stackrox/stackrox

ROX-33816: add db migration for soft deletes

migrator/runner/all.go
@@ -16,4 +16,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_220_to_m_221_add_deployment_hash_column"
 	_ "github.com/stackrox/rox/migrator/migrations/m_221_to_m_222_remove_v1_report_configs"
 	_ "github.com/stackrox/rox/migrator/migrations/m_222_to_m_223_remove_component_risk_records"
+	_ "github.com/stackrox/rox/migrator/migrations/m_223_to_m_224_add_deleted_at_index_and_set_deployment_state"
 )

davdhacs/stackrox-clean

migrator/runner/all.go
@@ -18,4 +18,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_222_to_m_223_remove_component_risk_records"
 	_ "github.com/stackrox/rox/migrator/migrations/m_223_to_m_224_add_deployment_type_and_enforcement_count_to_alerts"
 	_ "github.com/stackrox/rox/migrator/migrations/m_224_to_m_225_populate_deployment_containers_imageidv2"
+	_ "github.com/stackrox/rox/migrator/migrations/m_224_to_m_225_add_deleted_at_index_and_set_deployment_state"
 )