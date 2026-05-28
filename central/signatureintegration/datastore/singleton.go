stackrox/stackrox

ROX-30650: add key bundle updater (HTTP downloader)
Add an HTTP-based updater that downloads the Red Hat signing key bundle
from a configurable URL (ROX_REDHAT_SIGNING_KEY_BUNDLE_URL) and writes
it to the local file path watched by the watcher. Uses atomic file
writes, proxy-aware HTTP transport, and configurable polling interval.

Includes Kubernetes.groovy helpers for managing deployment env vars
and an E2E test that deploys an nginx server with a test bundle,
configures Central to download from it, and verifies the keys appear.

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

central/signatureintegration/datastore/singleton.go
@@ -24,8 +24,16 @@ var (
 	once     sync.Once
 	instance DataStore
 
+	bundleUpdater Stoppable
 	bundleWatcher Stoppable
 )
+
+// KeyBundleUpdater returns the key bundle updater for shutdown registration.
+// Returns nil if the updater was not started (e.g. no URL configured).
+// Must only be called after Singleton().
+func KeyBundleUpdater() Stoppable {
+	return bundleUpdater
+}
 
 // KeyBundleWatcher returns the key bundle watcher for shutdown registration.
 // Must only be called after Singleton().
@@ -52,6 +60,21 @@ func seedRedHatSignatureIntegration(siStore store.SignatureIntegrationStore) {
 	utils.Should(errors.Wrap(err, "seeding default Red Hat signature integration"))
 }
 
+func startKeyBundleUpdater() {
+	url := env.RedHatSigningKeyBundleURL.Setting()
+	if url == "" {
+		log.Info("ROX_REDHAT_SIGNING_KEY_BUNDLE_URL not set, key bundle updater will not start")
+		return
+	}
+
+	filePath := env.RedHatSigningKeyBundlePath.Setting()
+	interval := env.RedHatSigningKeyUpdateInterval.DurationSetting()
+
+	u := newKeyBundleUpdater(url, filePath, interval)
+	u.Start()
+	bundleUpdater = u
+}
+
 func startKeyBundleWatcher(siStore store.SignatureIntegrationStore) {
 	filePath := env.RedHatSigningKeyBundlePath.Setting()
 	interval := env.RedHatSigningKeyWatchInterval.DurationSetting()
@@ -68,6 +91,7 @@ func Singleton() DataStore {
 		seedRedHatSignatureIntegration(storage)
 		instance = New(storage, policyDataStore.Singleton())
 		startKeyBundleWatcher(storage)
+		startKeyBundleUpdater()
 	})
 	return instance
 }

davdhacs/stackrox-clean

central/signatureintegration/datastore/singleton.go
@@ -32,8 +32,16 @@ var (
 	once     sync.Once
 	instance DataStore
 
+	bundleUpdater Stoppable
 	bundleWatcher Stoppable
 )
+
+// KeyBundleUpdater returns the key bundle updater for shutdown registration.
+// Returns nil if the updater was not started (e.g. no URL configured).
+// Must only be called after Singleton().
+func KeyBundleUpdater() Stoppable {
+	return bundleUpdater
+}
 
 // KeyBundleWatcher returns the key bundle watcher for shutdown registration.
 // Must only be called after Singleton().
@@ -91,6 +99,20 @@ func keyBundleHandler(siStore store.SignatureIntegrationStore) filewatcher.Handl
 	}
 }
 
+func startKeyBundleUpdater() {
+	url := env.RedHatSigningKeyBundleURL.Setting()
+	if url == "" {
+		log.Info("ROX_REDHAT_SIGNING_KEY_BUNDLE_URL not set, key bundle updater will not start")
+		return
+	}
+
+	interval := env.RedHatSigningKeyUpdateInterval.DurationSetting()
+
+	u := newKeyBundleUpdater(url, redHatKeyBundlePath, interval)
+	u.Start()
+	bundleUpdater = u
+}
+
 func startKeyBundleWatcher(siStore store.SignatureIntegrationStore) {
 	interval := env.RedHatSigningKeyWatchInterval.DurationSetting()
 	if interval == 0 {
@@ -114,6 +136,7 @@ func Singleton() DataStore {
 		seedRedHatDefaultSignatureIntegration(storage) // must run before watcher; bundle file takes precedence on first tick
 		instance = New(storage, policyDataStore.Singleton())
 		startKeyBundleWatcher(storage)
+		startKeyBundleUpdater()
 	})
 	return instance
 }